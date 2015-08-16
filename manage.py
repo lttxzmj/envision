2ff0ede66f40a68d6ca160d3e8df94ac45bcb0ac
Jiangge Zhang <tonyseek@gmail.com>
2015-08-16 21:20:46 +0800

    add user model.

--- manage.py
+++ manage.py
@@ -2,10 +2,13 @@
 
 from __future__ import unicode_literals, absolute_import, print_function
 
-from flask_script import Manager
+import sys
+
+from flask_script import Manager, prompt_pass
 
 from envision.app import create_app
 from envision.ext import db
+from envision.models.user import User
 
 
 app = create_app()
@@ -24,6 +27,25 @@ def syncdb(destory=False, verbose=False):
     if destory:
         db.drop_all()
     db.create_all()
+
+
+@manager.command
+def useradd(email):
+    """Creates new user."""
+    if User.get_by_email(email):
+        abort('[%s] exists' % email)
+    try:
+        password = prompt_pass('Password [%s]' % email)
+    except KeyboardInterrupt:
+        abort('nothing changed.')
+    user = User.create(email, password, is_active=True)
+    db.session.commit()
+    print('[%s:%s] is created.' % (user.id, user.email))
+
+
+def abort(text, code=1):
+    print(text, file=sys.stderr)
+    sys.exit(code)
 
 
 if __name__ == '__main__':
